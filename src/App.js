f7ab9558fd3d70701a4e7aaa57f144dfcdd2f42d
David Patzke <david.patzke@wheregroup.com>
2021-01-08 16:43:31 +0100

    Style Infobox in print, add loading animation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MapLibreMap from './MapLibreMap';
-//import CookieConsent from "react-cookie-consent";
+import CookieConsent from "react-cookie-consent";
 
-/**    <CookieConsent>Diese Website verwendet Cookies – nähere Informationen dazu und zu Ihren Rechten als Benutzer finden Sie in unserer <a href="https://wheregroup.com/datenschutz/" target="_blank">  Datenschutzerklärung </a>Klicken Sie auf
-         „Ich stimme zu“, um Cookies zu akzeptieren und direkt unsere Website besuchen zu können.</CookieConsent> */
+
 function App() {
   return (
     <div className="App">
+      <CookieConsent buttonText="Okay"  buttonStyle={{ backgroundColor: "#B11E40", fontSize: "13px" }}>
+        Mit diesem Tool können Sie den Bewegungsradius für eine Stadt oder eine Adresse bestimmen.
+         Geben Sie dazu einfach den gewünschten Ort in das Suchfeld ein und wählen Sie einen Eintrag aus der erscheinenden Liste. 
+         Mit dem Drucksymbol können Sie die aktuelle Kartenansicht als PDF herunterladen und ausdrucken.</CookieConsent>
       <MapLibreMap />
 
 
