529484aa2918f5819c0703c580d192daa0714346
cioddi <maxtobiasweber@gmail.com>
2021-01-21 15:20:37 +0100

    refactor: Cleanup ShowVacSitesButton

--- src/components/ShowVacSitesButton.js
+++ src/components/ShowVacSitesButton.js
@@ -1,8 +1,11 @@
-import React, { useState,useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import MapContext from "../mapcomponents/MapContext";
 
 import Button from "react-bootstrap/Button";
-import {getEmptyFeatureCollection, getEmptyFeature} from '../mapcomponents/MapLibreMap/utils.js';
+import {
+  getEmptyFeatureCollection,
+  getEmptyFeature,
+} from "../mapcomponents/MapLibreMap/utils.js";
 
 const ShowVacSitesButton = () => {
   const [showVacSites, setShowVacSites] = useState(false);
@@ -12,34 +15,17 @@ const ShowVacSitesButton = () => {
 
   useEffect(() => {
     if (map) {
-      if (!showVacSites) {
-        map.setLayoutProperty(
-          "vaccination-point",
-          "visibility",
-          "none"
-        );
-        map.setLayoutProperty(
-          "vaccination-label",
-          "visibility",
-          "none"
-        );
-      } else {
-        map.setLayoutProperty(
-          "vaccination-point",
-          "visibility",
-          "visible"
-        );
-        map.setLayoutProperty(
-          "vaccination-label",
-          "visibility",
-          "visible"
-        );
+      let visibility = "visible";
+      if (showVacSites) {
+        visibility = "none";
       }
+      map.setLayoutProperty("vaccination-point", "visibility", visibility);
+      map.setLayoutProperty("vaccination-label", "visibility", visibility);
     }
   }, [showVacSites]);
 
   useEffect(() => {
-    if(map){
+    if (map) {
       getVacCenters();
     }
   }, [map]);
@@ -48,48 +34,53 @@ const ShowVacSitesButton = () => {
     fetch("./vaccination.json")
       .then((response) => response.json())
       .then((response) => {
-        const featureCollection = getEmptyFeatureCollection();
 
-        response.elements.forEach((e, i, l) => {
-          const feature = getEmptyFeature("Point", [e.lon, e.lat]);
-          Object.assign(feature.properties, e.tags);
-          featureCollection.features.push(feature);
-        });
+        if (!map.getSource("vaccination")) { // prevent hot reloading errors during development
 
-        map.addSource("vaccination", {
-          type: "geojson",
-          data: featureCollection,
-        });
+          const featureCollection = getEmptyFeatureCollection();
 
-        map.addLayer({
-          id: "vaccination-point",
-          type: "symbol",
-          source: "vaccination",
-          "icon-image": "marker",
-          layout: {
-            "icon-image": "syringe",
-            "icon-size": 0.06,
-            "icon-allow-overlap": true,
-            visibility: "none",
-          },
-        });
+          response.elements.forEach((e, i, l) => {
+            const feature = getEmptyFeature("Point", [e.lon, e.lat]);
+            Object.assign(feature.properties, e.tags);
+            featureCollection.features.push(feature);
+          });
 
-        map.addLayer({
-          id: "vaccination-label",
-          type: "symbol",
-          source: "vaccination",
-          layout: {
-            "text-field": ["get", "name"],
-            "text-font": ["Open Sans Regular"],
-            "text-variable-anchor": ["top", "bottom", "left", "right"],
-            "text-radial-offset": 0.5,
-            visibility: "none",
-            //'text-justify': 'auto',
-            //'text-allow-overlap': true
-          },
-        });
+          map.addSource("vaccination", {
+            type: "geojson",
+            data: featureCollection,
+          });
+
+          map.addLayer({
+            id: "vaccination-point",
+            type: "symbol",
+            source: "vaccination",
+            "icon-image": "marker",
+            layout: {
+              "icon-image": "syringe",
+              "icon-size": 0.06,
+              "icon-allow-overlap": true,
+              visibility: "none",
+            },
+          });
+
+          map.addLayer({
+            id: "vaccination-label",
+            type: "symbol",
+            source: "vaccination",
+            layout: {
+              "text-field": ["get", "name"],
+              "text-font": ["Open Sans Regular"],
+              "text-variable-anchor": ["top", "bottom", "left", "right"],
+              "text-radial-offset": 0.5,
+              visibility: "none",
+              //'text-justify': 'auto',
+              //'text-allow-overlap': true
+            },
+          });
+        }
       });
   };
+
 
 
   return (
